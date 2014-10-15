ced874b64c8a5a3c5274b2490f81e797e4e6cfd8
Jarvis Lin <jarvislin0202@gmail.com>
2014-10-16 00:29:52 +0800

    add info icon on actionbar

--- app/src/main/java/com/jarvislin/producepricechecker/SettingsActivity.java
+++ app/src/main/java/com/jarvislin/producepricechecker/SettingsActivity.java
@@ -3,14 +3,9 @@ package com.jarvislin.producepricechecker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Window;
-
-
-import java.lang.reflect.Method;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -38,32 +33,10 @@ public class SettingsActivity extends Activity {
     }
 
     @Override
-    public boolean onMenuOpened(int featureId, Menu menu)
-    {
-        if(featureId == Window.FEATURE_ACTION_BAR && menu != null){
-            if(menu.getClass().getSimpleName().equals("MenuBuilder")){
-                try{
-                    Method m = menu.getClass().getDeclaredMethod(
-                            "setOptionalIconsVisible", Boolean.TYPE);
-                    m.setAccessible(true);
-                    m.invoke(menu, true);
-                }
-                catch(NoSuchMethodException e){
-                    Log.e(TAG, "onMenuOpened", e);
-                }
-                catch(Exception e){
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        return super.onMenuOpened(featureId, menu);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search, menu);
+        inflater.inflate(R.menu.info, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
