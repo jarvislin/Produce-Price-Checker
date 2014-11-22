bebf57d8e3fdb1e746ef67370d98a43fe0826700
Jarvis Lin <jarvislin0202@gmail.com>
2014-11-22 11:17:46 +0800

    no message

--- app/src/main/java/com/jarvislin/producepricechecker/database/DBHelper.java
+++ app/src/main/java/com/jarvislin/producepricechecker/database/DBHelper.java
@@ -1,5 +1,6 @@
 package com.jarvislin.producepricechecker.database;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -7,6 +8,11 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Jarvis Lin on 2014/11/22.
  */
 public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                      int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
