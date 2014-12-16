b2e396e031d63c932253be955860a2fbef169060
Jarvis Lin <jarvislin0202@gmail.com>
2014-12-16 16:02:02 +0800

    add test

---
+++ app/src/androidTest/java/com/jarvislin/producepricechecker/IndexActivityTest.java
@@ -0,0 +1,37 @@
+package com.jarvislin.producepricechecker;
+
+import android.app.Application;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ApplicationTestCase;
+import android.widget.Button;
+
+import com.jarvislin.producepricechecker.IndexActivity;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class IndexActivityTest extends ActivityInstrumentationTestCase2<IndexActivity> {
+    IndexActivity mActivity;
+    Button mFruit;
+    Button mVegetable;
+    Button mSettings;
+
+    public IndexActivityTest() {
+        super(IndexActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mActivity = getActivity();
+        mFruit = (Button) mActivity.findViewById(R.id.fruit);
+        mVegetable = (Button) mActivity.findViewById(R.id.vegetable);
+        mSettings = (Button) mActivity.findViewById(R.id.settings);
+    }
+
+    public void testPreConditions() {
+        assertEquals(mFruit.isClickable(), true);
+        assertEquals(mVegetable.isClickable(), true);
+        assertEquals(mSettings.isClickable(), true);
+    }
+}
