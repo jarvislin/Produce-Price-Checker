bf268c0b9f494786b7358b9c9d36b937c603ec5a
Jarvis Lin <jarvislin0202@gmail.com>
2015-06-24 19:53:57 +0800

    DB migration bug fixed

--- app/src/main/java/database/ProduceDatabase.java
+++ app/src/main/java/database/ProduceDatabase.java
@@ -1,6 +1,10 @@
 package database;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.annotation.Migration;
+import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
 
 /**
  * Created by Jarvis Lin on 2015/6/13.
@@ -8,5 +12,5 @@ import com.raizlabs.android.dbflow.annotation.Database;
 @Database(name = ProduceDatabase.NAME, version = ProduceDatabase.VERSION)
 public class ProduceDatabase {
         public static final String NAME = "Produces";
-        public static final int VERSION = 1;
+        public static final int VERSION = 2;
 }
