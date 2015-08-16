1f96a4ab48dd83bad2366206afa575ac8655c6d0
Jarvis Lin <jarvislin0202@gmail.com>
2015-08-16 17:32:53 +0800

    update gradle

--- app/src/main/java/com/jarvislin/producepricechecker/ApiClient.java
+++ app/src/main/java/com/jarvislin/producepricechecker/ApiClient.java
@@ -21,6 +21,6 @@ import database.Produce;
 @Rest(converters = {FormHttpMessageConverter.class, GsonHttpMessageConverter.class})
 @Accept("application/json")
 public interface ApiClient extends RestClientErrorHandling {
-    @Post("http://produce.jarvislin.com/provider")
+    @Post("")
     ArrayList<ApiProduce> getData(MultiValueMap<String, String> map);
 }
