a1f781b54f02456a9101017f2743b4c857fbc2e6
Jarvis <jarvis@i-fit.com.tw>
2015-11-27 17:04:28 +0800

    add animation between activities

--- app/src/main/java/com/jarvislin/producepricechecker/BlankActivity.java
+++ app/src/main/java/com/jarvislin/producepricechecker/BlankActivity.java
@@ -5,39 +5,24 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
 
 import com.jarvislin.producepricechecker.page.History.HistoryPath;
-import com.jarvislin.producepricechecker.page.Index.IndexPath;
-import com.jarvislin.producepricechecker.page.Questions.QuestionsPath;
 import com.jarvislin.producepricechecker.path.FrameLayoutContainerView;
 import com.jarvislin.producepricechecker.path.GsonParceler;
-import com.jarvislin.producepricechecker.util.GoogleAnalyticsSender;
 import com.jarvislin.producepricechecker.util.ToolsHelper;
-import com.mikepenz.community_material_typeface_library.CommunityMaterial;
-import com.mikepenz.iconics.IconicsDrawable;
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
 
-import de.greenrobot.event.EventBus;
 import flow.Flow;
 import flow.FlowDelegate;
 import flow.History;
-import flow.path.Path;
 
 @EActivity(R.layout.activity_blank)
 public class BlankActivity extends AppCompatActivity implements Flow.Dispatcher, ActivityComponentHelper {
@@ -54,6 +39,7 @@ public class BlankActivity extends AppCompatActivity implements Flow.Dispatcher,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.savedInstanceState = savedInstanceState;
+        overridePendingTransition(R.anim.anim_page_left_in, R.anim.anim_page_left_out);
     }
 
     @AfterViews
@@ -166,5 +152,11 @@ public class BlankActivity extends AppCompatActivity implements Flow.Dispatcher,
     public boolean onCreateOptionsMenu(Menu menu) {
         container.onCreateOptionsMenu(this, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.anim_page_right_in, R.anim.anim_page_right_out);
     }
 }
